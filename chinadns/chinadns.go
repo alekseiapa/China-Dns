11d0212480a3bf16d5e971cc4e4d8c9c82a46fe3
Aleksei Apaseev <aleksei.apaseev@espressif.com>
2024-03-31 13:29:24 +0800

    refactor(chinadns): Move helper functions to utils

--- chinadns/chinadns.go
+++ chinadns/chinadns.go
@@ -6,7 +6,6 @@ import (
 	"github.com/miekg/dns"
 	"github.com/oschwald/geoip2-golang"
 	"github.com/sirupsen/logrus"
-	"net"
 	"strings"
 	"time"
 )
@@ -22,6 +21,11 @@ type DNServer struct {
 	dnsServers       [2]*dns.Server
 	chinaDomainCache *goc.Cache
 	responseCache    *goc.Cache
+}
+
+type cacheEntry struct {
+	putin time.Time
+	reply *dns.Msg
 }
 
 type DNServerError string
@@ -201,22 +205,6 @@ func (server *DNServer) LookupNetwork(request *dns.Msg, network string) (*dns.Ms
 	return response, server.config.SecondaryDNS, nil
 }
 
-func createServerFailureResponse(request *dns.Msg) *dns.Msg {
-	failureResponse := &dns.Msg{}
-	failureResponse.SetRcode(request, dns.RcodeServerFailure)
-	return failureResponse
-}
-
-func resolve(request *dns.Msg, upstreamDNS string, network string) (*dns.Msg, error) {
-	resolvedRequest := request.Copy()
-	resolvedRequest.Id = dns.Id()
-
-	client := &dns.Client{Net: network}
-	response, _, err := client.Exchange(resolvedRequest, upstreamDNS)
-
-	return response, err
-}
-
 func (server *DNServer) isResponsePolluted(response *dns.Msg) bool {
 	if containsARecord(response) {
 		chinaIP := containsChinaIP(response)
@@ -229,48 +217,6 @@ func (server *DNServer) isResponsePolluted(response *dns.Msg) bool {
 		return !china.(bool)
 	}
 	return false
-}
-
-func containsARecord(response *dns.Msg) bool {
-	for _, rr := range append(append(response.Answer, response.Ns...), response.Extra...) {
-		if _, ok := rr.(*dns.A); ok {
-			return true
-		}
-	}
-	return false
-}
-
-func containsChinaIP(response *dns.Msg) bool {
-	for _, rr := range append(append(response.Answer, response.Ns...), response.Extra...) {
-		if aRecord, ok := rr.(*dns.A); ok {
-			ip := aRecord.A.String()
-			if isChinaIP(ip) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func isChinaIP(ip string) bool {
-	record, err := geoIPDB.Country(net.ParseIP(ip))
-	if err != nil {
-		logger.Error(err)
-		return false
-	}
-	return record.Country.IsoCode == "CN"
-}
-
-func generateCacheKey(request *dns.Msg, network string) string {
-	query := request.Question[0]
-	key := query.Name + "_" + dns.TypeToString[query.Qtype]
-	if request.RecursionDesired {
-		key += "_RD"
-	} else {
-		key += "_NORD"
-	}
-	key += "_" + network
-	return key
 }
 
 func (server *DNServer) LookupCache(request *dns.Msg, network string) (*dns.Msg, bool) {
@@ -314,32 +260,6 @@ func (server *DNServer) setResponseCache(response *dns.Msg, network string) {
 		putin: time.Now(),
 		reply: response,
 	})
-}
-
-type cacheEntry struct {
-	putin time.Time
-	reply *dns.Msg
-}
-
-func subtractTTL(response *dns.Msg, deltaSeconds int) bool {
-	updateNeeded := false
-
-	updateTTL := func(rrs []dns.RR) {
-		for _, rr := range rrs {
-			newTTL := int(rr.Header().Ttl) - deltaSeconds
-			if newTTL <= 0 {
-				newTTL = minimumTTL
-				updateNeeded = true
-			}
-			rr.Header().Ttl = uint32(newTTL)
-		}
-	}
-
-	updateTTL(response.Answer)
-	updateTTL(response.Ns)
-	updateTTL(response.Extra)
-
-	return updateNeeded
 }
 
 func (server *DNServer) LookupHosts(request *dns.Msg) (*dns.Msg, bool) {
